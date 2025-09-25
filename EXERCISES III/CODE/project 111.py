17f79d8243c38c354a46f60188ed9dc4b61a3d69
mariejustineumutoniwase-spec <mariejustineumutoniwase@gmail.com>
2025-09-25 15:20:42 +0200

    Add files via upload

---
+++ EXERCISES III/CODE/project 111.py
@@ -0,0 +1,41 @@
+#stack:momo practice
+stack=[]
+stack.append("Enter PIN")
+stack.append("Enter Amount")
+stack.append("confirm")
+stack.pop()
+print("Remaining stack is:",stack)
+
+# practice UR pushes
+stack=[]
+stack.append("Assignment1")
+stack.append("Assignment2")
+stack.append("Assignment3")
+stack.pop()
+stack.pop()
+print("Remaining stack is:",stack)
+# practice at Nyabugogo 7 buses queue
+Queue=[]
+Queue.append("Bus 1")
+Queue.append("Bus 2")
+Queue.append("Bus 3")
+Queue.append("Bus 4")
+Queue.append("Bus 5")
+Queue.append("Bus 6")
+Queue.append("Bus 7")
+Queue.pop(0)
+Queue.pop(0)
+Queue.pop(0)
+Queue.pop(0)
+print("Remaining bus in front is:",Queue[0])
+
+# practical at RRA 6 citizens queue
+Queue=[]
+Queue.append("Citizen 1")
+Queue.append("Citizen 2")
+Queue.append("Citizen 3")
+Queue.append("Citizen 4")
+Queue.append("Citizen 5")
+Queue.append("Citizen 6")
+Queue.pop(0)
+print("The citizen who is the second:",Queue[0])
